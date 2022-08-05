b03146474909f3c11b3ab263d39b8e8da9e21362
Harish <harish.chanemougam@3wa.io>
2022-08-05 16:11:28 +0200

    initial

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour    
+{
+    bool _play;
+    bool _continue;
+    bool _load;
+    bool _newGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
